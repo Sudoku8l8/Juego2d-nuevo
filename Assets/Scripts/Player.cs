cff4d1a03d1651a6bfc2b1cd94dc1275a6075284
Sudoku <ultrashare8l8@gmail.com>
2026-09-09 18:07:47 -0500

    Agregando un script para el movimiento del personaje

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed =5;
+    private Rigidbody2D rb2D;
+
+    private float  move;
+
+
     void Start()
     {
-        
+        rb2D= GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move= Input.GetAxisRaw("Horizontal");
+        rb2D.linearVelocity =new Vector2(move*speed, rb2D.linearVelocity.y);
     }
 }
